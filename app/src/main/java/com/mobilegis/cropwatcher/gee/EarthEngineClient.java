afa0035c2bda91fd809a2f04825b20c375d53883
CropWatcher Developer <developer@cropwatcher.com>
2026-06-13 19:37:44 +0700

    Import java.util.List in EarthEngineClient

--- app/src/main/java/com/mobilegis/cropwatcher/gee/EarthEngineClient.java
+++ app/src/main/java/com/mobilegis/cropwatcher/gee/EarthEngineClient.java
@@ -13,6 +13,7 @@ import com.mobilegis.cropwatcher.data.entity.Plot;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
